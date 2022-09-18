1ff983baf2f936d200c1ba37cdfeb761d9b4715b
Alexey Belkov <lexabelco2013@yandex.ru>
2022-09-18 15:04:33 +0500

    style: изменить отступы для pixel perfect

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-let popup = document.querySelector('.popup');
-let formPopup = popup.querySelector('.popup__container');
-let closePopupBtn = formPopup.querySelector('.popup__close-btn');
-let nameInput = formPopup.querySelector('.popup__input_field_name');
-let descriptionInput = formPopup.querySelector('.popup__input_field_description');
-let profile = document.querySelector('.profile');
-let openPopupBtn = profile.querySelector('.profile__edit-btn');
-let nameUser = profile.querySelector('.profile__user-name');
-let descriptionUser = profile.querySelector('.profile__user-description');
+const popup = document.querySelector('.popup');
+const formPopup = popup.querySelector('.popup__container');
+const closePopupBtn = formPopup.querySelector('.popup__close-btn');
+const nameInput = formPopup.querySelector('.popup__input_field_name');
+const descriptionInput = formPopup.querySelector('.popup__input_field_description');
+const profile = document.querySelector('.profile');
+const openPopupBtn = profile.querySelector('.profile__edit-btn');
+const nameUser = profile.querySelector('.profile__user-name');
+const descriptionUser = profile.querySelector('.profile__user-description');
 
 function openPopup () {
   popup.classList.add('popup_opened');
