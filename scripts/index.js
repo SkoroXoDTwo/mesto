8fd57e83e1f4f25b488c5871f9ec9275ec8293d8
Alexey Belkov <lexabelco2013@yandex.ru>
2022-09-29 22:46:53 +0500

    feat: добавить подгрузку карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,33 @@ const profile = document.querySelector('.profile');
 const openPopupBtn = profile.querySelector('.profile__edit-btn');
 const nameUser = profile.querySelector('.profile__user-name');
 const descriptionUser = profile.querySelector('.profile__user-description');
+const gallery = document.querySelector('.gallery__list');
+const initialGallery = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 function openPopup () {
   popup.classList.add('popup_opened');
@@ -25,6 +52,19 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
+function renderGallery (containerItem) {
+  const galleryItemTemplate = document.querySelector('#gallery-item-template').content;
+  containerItem.forEach((item) => {
+    const galleryItem = galleryItemTemplate.querySelector('.gallery__list-element').cloneNode(true);
+    const nameItem = galleryItem.querySelector('.gallery__name');
+    const linkItem = galleryItem.querySelector('.gallery__photo');
+    nameItem.textContent = item.name;
+    linkItem.src = item.link;
+    gallery.append(galleryItem);
+  });
+
+}
+renderGallery(initialGallery);
 formPopup.addEventListener('submit', formSubmitHandler);
 openPopupBtn.addEventListener('click', openPopup);
 closePopupBtn.addEventListener('click', closePopup);
